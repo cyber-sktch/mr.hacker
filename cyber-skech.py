328fe87a17a9f7cda7d093c87442799ed91eef29
Termux cyber world <66860492+cyber-sktch@users.noreply.github.com>
2020-07-03 14:52:44 +0530

    mr.hacker
    hay,bro

---
+++ cyber-skech.py
@@ -0,0 +1,203 @@
+#crated by Joan Salomon and Sergio Dominguez
+import random
+import hashlib
+import time 
+from colored import fg,bg,attr
+color = fg('green') 
+resrt = attr('reset')
+print(color  + "               _____________________________________________________________________________________")
+print(color  + "               |                                  ___________________                              |")
+print(color  + "               |                                 |    __      __     |                             |")
+print(color  + "               |                                 |   /__\    /__\    |                             |")
+print(color  + "               |                                 \        \_         /                             |")
+print(color  + "               |                                  \                 /                              |")
+print(color  + "               |                                   \  \__________/ /                               |")
+print(color  + "               |                                    \             /                                |")
+print(color  + "               |                                     \   \____/  /                                 |")
+print(color  + "               |                                      \_varshil_/                                  |")
+print(color  + "               |___________________________________________________________________________________|") 
+
+print(color + "                               hi,you are hacker")
+
+number = (input("                               yes and no:-"))
+             
+
+print(color  + "               ____________________________________________________________________________________ ")
+print(color  + "               |                                  ___________________                              |")
+print(color  + "               |                                 |    __      __     |                             |")
+print(color  + "               |                                 |   /__\    /__\    |                             |")
+print(color  + "               |                                 \        \_         /                             |")
+print(color  + "               |                                  \                 /                              |")
+print(color  + "               |                                   \  \__________/ /                               |")
+print(color  + "               |                                    \             /                                |")
+print(color  + "               |                                     \   \____/  /                                 |")
+print(color  + "               |                                      \_varshil_/                                  |")
+print(color  + "               |___________________________________________________________________________________|") 
+
+
+
+
+
+
+
+
+
+print(" ________________________________________________________________________________________________________________________")
+print(" |      ___    __      _                                                          _________     __                       |")                                                                    
+print(" |      |  \  /  |    |  \          |     |     ______       ________  |    /     |            |     \                   |")
+print(" |      |   \/   |    |  /          |_____|    /      \    /           |   /      |            |      \                  |")
+print(" |      |        |    |  \          |     |   /        \  /            |  /       | _______    |      /                  |")
+print(" |      |        |        \     .;  |     |  /____      \ \            |  \       |            |   \ /                   |")
+print(" |                         \        |     | /            \ \ ________  |   \      |            |    \                    |")
+print(" |                                                        \            |    \     | ________   |     \                   |")
+print(" |                                                                     |                              \                  |")
+print(" |_______________________________________________________________________________________________________________________|") 
+            
+            
+            
+            
+    
+
+print(color  + "               _____________________________________________________________________________________")
+print(color  + "               |                                  ___________________                              |")
+print(color  + "               |                                 |    __      __     |                             |")
+print(color  + "               |                                 |   /__\    /__\    |                             |")
+print(color  + "               |                                 \        \_         /                             |")
+print(color  + "               |                                  \                 /                              |")
+print(color  + "               |                                   \  \__________/ /                               |")
+print(color  + "               |                                    \             /                                |")
+print(color  + "               |                                     \   \____/  /                                 |")
+print(color  + "               |                                      \_varshil_/                                  |")
+print(color  + "               |___________________________________________________________________________________|") 
+
+print(color  + "               _____________________________________________________________________________________")
+print(color  + "               |                                  ___________________                              |")
+print(color  + "               |                                 |    __      __     |                             |")
+print(color  + "               |                                 |   /__\    /__\    |                             |")
+print(color  + "               |                                 \        \_         /                             |")
+print(color  + "               |                                  \                 /                              |")
+print(color  + "               |                                   \  \__________/ /                               |")
+print(color  + "               |                                    \             /                                |")
+print(color  + "               |                                     \   \____/  /                                 |")
+print(color  + "               |                                      \_varshil_/                                  |")
+print(color  + "               |___________________________________________________________________________________|") 
+
+print(color +  "                                this tool is run ")
+name = input("yes and no:-")
+print("________________________________________________________________________")
+print("|                                                                      |")
+print("|                                                                      |")
+print("|                yes type 1 and no type you:                           |")
+print("|                                                                      |")
+print("|______________________________________________________________________|")
+int(input("enter a number:-"))
+print("_________________________________________________________________________ ")
+print("|    [+] this tool a phishing                                           |")
+print("|                  [+] enjoy to my tool                                 |")
+print("|                                                                       |")
+print("|                                                                       |")
+print("|                                                                       |")
+print("|             [1] facebook               [4] whatsapp                   |")
+print("|             [2] instagram              [5] google                     |")
+print("|             [3] paypal                 [6] PASSWORD CRACKER           |")
+print("|                                                                       |") 
+print("|_______________________________________________________________________|")
+varshil = input()
+if varshil == "1":
+    print(" varshil")  
+elif varshil == "2":
+    print("varshil")
+elif varshil == "3":
+    print("varshil") 
+elif varshil == "4":
+    print("pc")
+elif varshil == "5":
+    print("tebalet")
+elif varshil == "6":
+    print("**************PASSWORD CRACKER******************") 
+
+varshik = 100
+varshil = 100
+pokemon2 = (" Instagram-Followers", " Google-TFO", " Paytm-TFO")
+aimy = random.choice(pokemon2)
+luck = random.randint(1, 2)
+
+while varshil > 1:
+    
+    print(" ")
+    print("")
+    print("      new tool")
+    print("      [1] Instagram-Followers")
+    print("      [2] Google-TFO")
+    print("      [3] Paytm-TFO ")
+    
+    yeet = input()
+    if yeet == "1":
+        print(name +"Instagram-Followers"+ aimy +"!")
+    elif yeet == "2":
+        if luck == 4:
+            print(name + " hi i am varshil and you are hacker " + aimy +"!")
+            unknown = unknown - 1
+            print(aimy +" v.p", str(int(unknown))+" v.p hellp")
+    else:
+            print(name +" has fallat...")
+    
+        
+            print(player +" v.p ", str(int(player))+" v.p hellp")
+    
+            print("NUMERO INVALID")
+    
+    if varshil < 1.0:
+            
+            print(name + " hey,i am varshil and your name")
+            
+            break
+        
+    elif varshil < 1.0:
+        sleep(1)
+        print(name + " hey, MR.hacker this varshil prajapti")
+        sleep(2)
+        break
+
+    ai = random.randint(7, 9)
+    print(" ")
+    print(aimy +" this my tool is good")
+    print(" ")
+    
+while varshil:
+        if ai == 7:
+            print(aimy +" hi, hey , i am varshil prajapti"+ name +"!")
+            varshil = varshil - 1
+            print(name +" v.p ", str(int(player))+" v.p hellp")
+      
+        elif ai == 1:
+            
+            if luck == 4:
+                print(aimy +" hey, my sisuka hi my hacker feind"+ name +"!")
+                player = player - 1
+                print(name +" v.p ", str(int(player))+" v.p hellp")
+                
+            else:
+                print(aimy +" ha fallat...")
+
+        elif ai == 9:
+            print(aimy +" hi i am varshil prajapati1!")
+            varshik = varshik + 1
+            print(unknown +" v.p ",
+             str(int(unknown))+" v.p hellp")
+            print(" ")
+            
+        if varshil < 1.0:
+            sleep(1)
+            print(name + " varshil hellp")
+            sleep(2)
+            break
+        
+        elif varshik < 1.0:
+            sleep(1)
+            print(name + " hey i am varshil ")
+            sleep(2)
+            break
+            
+            
+            
